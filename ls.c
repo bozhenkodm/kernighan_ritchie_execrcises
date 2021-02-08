a8812142dd1c4d255bca65ef30606bd4d59e6ae7
bozhenkodm <skeeph@gmail.com>
2021-02-08 23:03:59 +0300

    add dir entries sort

--- ls.c
+++ ls.c
@@ -1,3 +1,5 @@
+#define _DEFAULT_SOURCE
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -6,9 +8,30 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <ctype.h>
 
 #define dprint(expr) printf(#expr " = %g\n", expr)
 #define sprint(expr) printf(#expr " = %s\n", expr)
+#define MAXLENGTH 1000
+
+int natural_comparison(const struct dirent **a, const struct dirent **b);
+
+int natural_comparison(const struct dirent **a, const struct dirent **b)
+{
+    const char *a_name, *b_name;
+    a_name = (*a)->d_name;
+    b_name = (*b)->d_name;
+    if (*a_name == '.')
+    {
+        a_name++;
+    }
+    if (*b_name == '.')
+    {
+        b_name++;
+    }
+
+    return strcasecmp(b_name, a_name);
+}
 
 int main(int argc, char **argv)
 {
@@ -24,16 +47,23 @@ int main(int argc, char **argv)
     }
     if (S_ISDIR(mystat->st_mode))
     {
-        DIR *directory = opendir(path);
-        char *filename, *dir_copy;
+        // DIR *directory = opendir(path);
+        char dir_path_copy[MAXLENGTH];
+        int directory_entries_number;
 
-        struct dirent *dir_entry;
-        while ((dir_entry = readdir(directory)) != NULL)
+        struct dirent **sorted_dir_entries;
+
+        directory_entries_number = scandir(path, &sorted_dir_entries, NULL, natural_comparison);
+
+        while (directory_entries_number--)
         {
-            filename = dir_entry->d_name;
-            filename = strcat(path, filename);
-            stat(filename, mystat);
-            printf("%-60s%ld\n", filename, mystat->st_size);
+            struct dirent *dir_entry;
+            strcpy(dir_path_copy, path);
+            strcat(dir_path_copy, "/");
+            dir_entry = sorted_dir_entries[directory_entries_number];
+            strcat(dir_path_copy, dir_entry->d_name);
+            stat(dir_path_copy, mystat);
+            printf("%-60s%ld\n", dir_path_copy, mystat->st_size);
         }
     }
     else
