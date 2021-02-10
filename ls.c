c649f785c8e8af9c01d4787ef26136f19da3a4f6
bozhenkodm <skeeph@gmail.com>
2021-02-10 20:44:33 +0300

    add permissions print

--- ls.c
+++ ls.c
@@ -15,6 +15,7 @@
 #define MAXLENGTH 1000
 
 int natural_comparison(const struct dirent **a, const struct dirent **b);
+void print_permission_string(unsigned long st_mode);
 
 int natural_comparison(const struct dirent **a, const struct dirent **b)
 {
@@ -33,19 +34,61 @@ int natural_comparison(const struct dirent **a, const struct dirent **b)
     return strcasecmp(b_name, a_name);
 }
 
+void print_permission_string(unsigned long st_mode)
+{
+    switch (st_mode & S_IFMT)
+    {
+    case S_IFSOCK:
+        printf("s");
+        break;
+    case S_IFLNK:
+        printf("l");
+        break;
+    case S_IFREG:
+        printf("-");
+        break;
+    case S_IFBLK:
+        printf("b");
+        break;
+    case S_IFDIR:
+        printf("d");
+        break;
+    case S_IFCHR:
+        printf("c");
+        break;
+    case S_IFIFO:
+        printf("p");
+        break;
+
+    default:
+        printf("hz");
+        break;
+    }
+    printf((st_mode & S_IRUSR) ? "r" : "-");
+    printf((st_mode & S_IWUSR) ? "w" : "-");
+    printf((st_mode & S_IXUSR) ? "x" : "-");
+    printf((st_mode & S_IRGRP) ? "r" : "-");
+    printf((st_mode & S_IWGRP) ? "w" : "-");
+    printf((st_mode & S_IXGRP) ? "x" : "-");
+    printf((st_mode & S_IROTH) ? "r" : "-");
+    printf((st_mode & S_IWOTH) ? "w" : "-");
+    printf((st_mode & S_IXOTH) ? "x" : "-");
+    printf(" ");
+}
+
 int main(int argc, char **argv)
 {
     (void)argc;
-    struct stat *mystat = malloc(sizeof *mystat);
+    struct stat *os_entry_stat = malloc(sizeof *os_entry_stat);
     const char *path = argv[1];
 
-    stat(path, mystat);
+    stat(path, os_entry_stat);
     if (errno)
     {
         printf("errno = %s\n", strerror(errno));
         return 1;
     }
-    if (S_ISDIR(mystat->st_mode))
+    if (S_ISDIR(os_entry_stat->st_mode))
     {
         // DIR *directory = opendir(path);
         char dir_path_copy[MAXLENGTH];
@@ -62,12 +105,14 @@ int main(int argc, char **argv)
             strcat(dir_path_copy, "/");
             dir_entry = sorted_dir_entries[directory_entries_number];
             strcat(dir_path_copy, dir_entry->d_name);
-            stat(dir_path_copy, mystat);
-            printf("%-60s%ld\n", dir_path_copy, mystat->st_size);
+            stat(dir_path_copy, os_entry_stat);
+            print_permission_string(os_entry_stat->st_mode);
+            printf("%-60s%ld\n", dir_path_copy, os_entry_stat->st_size);
         }
     }
     else
     {
-        printf("%-20s%ld\n", argv[1], mystat->st_size);
+        print_permission_string(os_entry_stat->st_mode);
+        printf("%-60s%ld\n", argv[1], os_entry_stat->st_size);
     }
 }
